9a10d61c6925ee00477e2746197a01bac2d590ad
Filip Luśnia <uszaty223@onet.pl>
2020-09-14 10:31:40 +0200

    tweaks

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import {Invoice} from './classes/Invoice.js';
+import {ListTemplate} from './classes/ListTemplate.js';
 import {Payment} from './classes/Payment.js';
 import {HasFormatter} from './interfaces/HasFormatter';
 
@@ -9,16 +10,23 @@ const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul); 
+
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault(); 
+
+    let values: [string, string, number];
+    values = [tofrom.value, details.value, amount.valueAsNumber];
 
     let doc: HasFormatter;
 
     type.value === 'invoice' 
     ? 
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+        doc = new Invoice(...values)
     :
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber) 
+        doc = new Payment(...values) 
+    ;
 
-        console.log(doc)
+    list.render(doc, type.value, 'end');
 })
